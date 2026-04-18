d966f004e8211d1a3d9b8617cfbe5c4d28bf9fb5
Hridoy <hridoyncs0464@gmail.com>
2026-04-18 23:31:32 +0600

    completed

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import Banner from '../../Component/Header/Banner';
-import Categories from '../../Component/Categories/Categories';
-import WhyChooseUs from '../../Component/Sections/Whychooseus';
-import Reviews from '../../Component/Sections/Reviews';
-import LocationMap from '../../Component/Sections/LocationMap';
-import SignatureDishSlider from '../../Component/Sections/SignatureDishSlider';
+import React from "react";
+import Banner from "../../Component/Header/Banner";
+import Categories from "../../Component/Categories/Categories";
+import Reviews from "../../Component/Sections/Reviews";
+import LocationMap from "../../Component/Sections/LocationMap";
+import SignatureDishSlider from "../../Component/Sections/SignatureDishSlider";
+import WhyChooseUs from "../../Component/Sections/WhyChooseUs";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <SignatureDishSlider /> 
-            <Categories></Categories>
-          <WhyChooseUs></WhyChooseUs>
-          <Reviews></Reviews>
-          <LocationMap/>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <SignatureDishSlider />
+      <Categories></Categories>
+      <WhyChooseUs></WhyChooseUs>
+      <Reviews></Reviews>
+      <LocationMap />
+    </div>
+  );
 };
 
 export default Home;
+ 
